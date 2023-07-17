82e00bf7f67e62951f172cafc184863900157fa4
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2023-07-17 13:28:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,29 +8,36 @@ from pycaret.classification import *
 #import pickle
 import numpy as np
 import pandas as pd
+
 #import pickle
+
+saved_final_rf = load_model('model')
+g = Github("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")
+
+user = g.get_user()
+
+repository = user.get_repo('pres')
+
+file_content = repository.get_contents('jun23.csv')
+
+bytes_data=file_content.decoded_content
+
+s=str(bytes_data,'utf-8')
+
+file = open("data.txt","w")
+
+file.write(s)
+
+df = pd.read_csv('data.txt')
+df.rename(columns = {'Customer Name':"Party_Name","Plant":"Warehouse","Target Quantity":"Net Weight"},inplace = True)
+
+@app.route('/')
+def home1():	
+    name1 = [x for x in df.Warehouse]
+    return render_template('index2.html', name1=name1)
 
 @app.route('/')
 def home():	
-    saved_final_rf = load_model('model')
-    g = Github("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")
-
-    user = g.get_user()
-
-    repository = user.get_repo('pres')
-
-    file_content = repository.get_contents('jun23.csv')
-
-    bytes_data=file_content.decoded_content
-
-    s=str(bytes_data,'utf-8')
-
-    file = open("data.txt","w")
-
-    file.write(s)
-
-    df = pd.read_csv('data.txt')
-    df.rename(columns = {'Customer Name':"Party_Name","Plant":"Warehouse","Target Quantity":"Net Weight"},inplace = True)
     name = [x for x in df.Party_Name]
     return render_template('index2.html', name=name)
     

87a60d6f3328fd393c4095ff1ac14c9608dcf9a1
vikrantzerozero8 <116074891+vikrantzerozero8@users.noreply.github.com>
2023-07-30 16:07:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def main():
                     df = pd.read_excel(uploadedFile,  index_col=0)
                 except:      
                     df = pd.DataFrame(uploadedFile)
-        df.rename(columns = {'Customer Name':"Party_Name","Plant":"Warehouse","Target Quantity":"Net_Weight","Freight Rate":"Freight_Rate"},inplace = True)
+        df = df.rename(columns = {'Customer Name':"Party_Name","Plant":"Warehouse","Target Quantity":"Net_Weight","Freight Rate":"Freight_Rate"},inplace = True)
         name = {"GIR":[x for x in df.Party_Name if (True for NUM in df.Warehouse if NUM == "GIR")] ,
             "LKDRM2":[x for x in df.Party_Name if (True for NUM in df.Warehouse if NUM == "LKDRM2")],
             "RSDSH":[x for x in df.Party_Name if (True for NUM in df.Warehouse if NUM == "RSDSH")],

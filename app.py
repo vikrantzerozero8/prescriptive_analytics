c840356a76e4eb7863014dc28f46972e0aed9ae5
vikrantzerozero8 <116074891+vikrantzerozero8@users.noreply.github.com>
2023-07-30 16:56:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,9 @@ def main():
         try:
 
             df=pd.read_csv(uploadedFile,  index_col=0)
-            df.rename(columns = {'Customer Name':"Party_Name","Plant":"Warehouse","Target Quantity":"Net_Weight","Freight Rate":"Freight_Rate"},inplace = True)
+            df.columns = df.columns.str.replace(' ', '')
+            df.columns = df.columns.str.lower()
+            df.rename(columns = {'Customername':"Party_Name","plant":"Warehouse","targetquantity":"Net_Weight","freightrate":"Freight_Rate"},inplace = True)
             st.write(df)
             
         except:

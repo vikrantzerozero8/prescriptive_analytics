d74841f40ba586302160f858323f9ac7a01abb00
vikrantzerozero8 <116074891+vikrantzerozero8@users.noreply.github.com>
2023-07-30 18:01:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def main():
             
         except:
                 try:
-                    df = pd.read_excel(uploadedFile, encoding='utf-8')
+                    df = pd.read_excel(uploadedFile)
                     
                 except:      
                     df = pd.DataFrame(uploadedFile)
@@ -37,8 +37,8 @@ def main():
     else:
         st.sidebar.warning("you need to upload a csv or excel file.")
     if uploadedFile is not None : 
-        #df.columns = df.columns.str.replace(' ', '')
-        #df.columns = df.columns.str.lower()
+        df.columns = df.columns.str.replace(' ', '')
+        df.columns = df.columns.str.lower()
         st.write(df)
         df.rename(columns = {'customername':"Party_Name","plant":"Warehouse","targetquantity":"Net_Weight","freightrate":"Freight_Rate","distance":"Distance"},inplace = True)
         

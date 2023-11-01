3aa9532abfca667bfdaebf97f792e1f3899fd8ba
vikrantzerozero8 <116074891+vikrantzerozero8@users.noreply.github.com>
2023-11-01 15:16:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -136,8 +136,8 @@ def main():
         decision_var_df.shape
         cost_mat.shape
         # Calculate transportation cost after optimization
-        total_after_opt = [decision_var_df.loc[w][p] * cost_mat.loc[w][p]  for w in distance_matrix.index for p in distance_matrix.columns]
-        
+        total_after_opt = [decision_var_df.loc[w][p] * cost_mat.loc[w][p] .sum().sum() for w in warehouses for p in party_names]
+        total1 = sum(total_after_opt)
         # Multiplying the DataFrames
         result_df = decision_var_df.dot(cost_mat)
         
